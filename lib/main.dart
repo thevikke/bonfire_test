5a972cc9e5ca2c1c7d246bb58d0454bae615adf3
thevikke <31620078+thevikke@users.noreply.github.com>
2023-09-24 11:52:58 +0300

    feat(Init): Add MVP for the project.

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:bonfire/bonfire.dart';
+import 'package:bonfire_test/my_enemy.dart';
+import 'package:bonfire_test/my_player.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb) {
+    await Flame.device.setLandscape();
+    await Flame.device.fullScreen();
+  }
+
+  runApp(
+    const MaterialApp(home: MyGame()),
+  );
+}
+
+class MyGame extends StatelessWidget {
+  const MyGame({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BonfireWidget(
+      joystick: Joystick(
+        keyboardConfig: KeyboardConfig(),
+        directional: JoystickDirectional(),
+      ),
+      map: WorldMapByTiled(
+        'tiled/mapa2.json',
+        forceTileSize: Vector2(32, 32),
+        objectsBuilder: {
+          'goblin': (properties) => MyEnemy(properties.position),
+        },
+      ),
+      player: MyPlayer(Vector2(140, 140)),
+      cameraConfig: CameraConfig(moveOnlyMapArea: true, zoom: 1.5),
+      backgroundColor: const Color.fromARGB(255, 10, 53, 89),
+    );
+  }
+}
