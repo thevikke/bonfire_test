5e8103608add722685e5ab21cccce2b9089fce77
thevikke <31620078+thevikke@users.noreply.github.com>
2023-10-12 11:10:50 +0300

    feat(Particles): Add example with particle generation inside my_player.

--- lib/my_player.dart
+++ lib/my_player.dart
@@ -1,5 +1,12 @@
+import 'dart:math';
+
 import 'package:bonfire/bonfire.dart';
 import 'package:bonfire_test/shared/util/player_sprite_sheet.dart';
+import 'package:flutter/material.dart';
+
+Random rnd = Random();
+
+Vector2 randomVector2() => (Vector2.random(rnd) - Vector2.random(rnd)) * 200;
 
 class MyPlayer extends SimplePlayer with ObjectCollision {
   MyPlayer(Vector2 position)
@@ -17,5 +24,34 @@ class MyPlayer extends SimplePlayer with ObjectCollision {
         ],
       ),
     );
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    gameRef.add(
+      ParticleSystemComponent(
+        particle: Particle.generate(
+          count: 1,
+          lifespan: 0.1,
+          generator: (i) => AcceleratedParticle(
+            acceleration: getRandomVector(),
+            speed: getRandomVector(),
+            position: (position.clone() + Vector2(size.x / 2, size.y - size.y)),
+            child: CircleParticle(
+              radius: 1,
+              paint: Paint()..color = Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  final _random = Random();
+
+  Vector2 getRandomVector() {
+    return (Vector2.random(_random) - Vector2(0.5, -1)) * 200;
   }
 }
